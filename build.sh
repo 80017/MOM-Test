837ef45f0ccfb851744a8d3b0218527b869dd7b8
80017 <arpitap@officebeacon.com>
2017-11-30 11:19:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
--d '{"inServiceStrategy":{"launchConfig": {"imageUuid":"docker:nginx"}},"toServiceStrategy":null}' \
-'http://162.209.122.250:8080/v2-beta/projects/1a5/services/1s380?action=upgrade'
+-d '{"inServiceStrategy":{"launchConfig": {"imageUuid":"docker:80017/todo-db"}},"toServiceStrategy":null}' \
+'http://162.209.122.250:8080/v2-beta/projects/1a5/services/1s416?action=upgrade'
